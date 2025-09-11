e53e1fd7a0a2f884296bee97df3ceb254c7ddc92
Pepelocotango <pepelocotango@gmail.com>
2025-09-12 00:11:57 +0200

    Revert "test: Remove logger from GoogleSettingsModal"
    This reverts commit dc17daba9f55ad688ebec92150aceefdefc05d86.

--- src/components/modals/GoogleSettingsModal.tsx
+++ src/components/modals/GoogleSettingsModal.tsx
@@ -39,6 +39,8 @@ const GoogleSettingsModal: React.FC<GoogleSettingsModalProps> = ({ onClose, show
 
   logger.info('[GoogleSettingsModal Render] State:', storeState);
 
+  logger.info('[GoogleSettingsModal Render] State:', storeState);
+
   useEffect(() => {
     fetchAndLoadConfig();
   }, []);
