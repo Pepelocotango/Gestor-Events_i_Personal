a788a1eff9855b8a7d9b8099fc58f8db7947c536
Pepelocotango <pepelocotango@gmail.com>
2025-12-11 13:46:27 +0100

    Update PdfPreviewModal.tsx
    canvi  de icona espera vista prèvia fitxa de bolo

--- src/components/modals/PdfPreviewModal.tsx
+++ src/components/modals/PdfPreviewModal.tsx
@@ -24,8 +24,7 @@ const PdfPreviewModal: React.FC<PdfPreviewModalProps> = ({ onClose, pdfUrl, titl
       </div>
       <div className="flex-grow relative">
         {isLoading && (
-          <div className="absolute inset-0 flex flex-col justify-center items-center bg-white dark:bg-gray-800 rounded">
-            <svg className="animate-spin h-8 w-8 text-primary mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <div className="absolute inset-0 z-10 flex flex-col justify-center items-center bg-white dark:bg-gray-800 rounded">            <svg className="animate-spin h-8 w-8 text-primary mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
@@ -36,8 +35,7 @@ const PdfPreviewModal: React.FC<PdfPreviewModalProps> = ({ onClose, pdfUrl, titl
           src={pdfUrl}
           width="100%"
           height="100%"
-          className={`border border-gray-300 dark:border-gray-700 rounded transition-opacity duration-300 ${isLoading ? 'opacity-0' : 'opacity-100'}`}
-          title="Vista Prèvia PDF"
+          className={`relative z-0 border border-gray-300 dark:border-gray-700 rounded transition-opacity duration-300 ${isLoading ? 'opacity-0' : 'opacity-100'}`}          title="Vista Prèvia PDF"
           onLoad={() => setIsLoading(false)}
         />
       </div>
