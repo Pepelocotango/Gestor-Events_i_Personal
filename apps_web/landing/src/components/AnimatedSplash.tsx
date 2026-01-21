4dbee22d0298c0f24d96c52cc483651ca0f361c3
Pepelocotango <pepelocotango@gmail.com>
2026-01-21 14:51:31 +0100

    splash screen in web

---
+++ apps_web/landing/src/components/AnimatedSplash.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+
+// Importem els fotogrames de l'animació
+const frames = [
+  '/splash/frame_1.png',
+  '/splash/frame_2.png',
+  '/splash/frame_3.png',
+  '/splash/frame_4.png',
+  '/splash/frame_5.png',
+  '/splash/frame_6.png',
+  '/splash/frame_7.png',
+  '/splash/frame_8.png',
+  '/splash/frame_9.png',
+];
+
+export default function AnimatedSplash() {
+  const [currentFrame, setCurrentFrame] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    // Canvia el fotograma cada 200ms
+    const animationInterval = setInterval(() => {
+      setCurrentFrame((prevFrame) => (prevFrame + 1) % frames.length);
+    }, 200);
+
+    return () => {
+      clearInterval(animationInterval);
+    };
+  }, []);
+
+  return (
+    <div className="absolute inset-0 flex items-center justify-center overflow-visible" style={{
+      transform: 'scale(1.5)',
+      transformOrigin: 'center',
+      width: '100%',
+      height: '100%'
+    }}>
+      <img 
+        src={frames[currentFrame]} 
+        alt="Animació de càrrega" 
+        className="w-full h-full object-contain"
+        style={{ 
+          mixBlendMode: 'multiply',
+          width: '100%',
+          height: '100%',
+          objectFit: 'contain'
+        }}
+      />
+    </div>
+  );
+}
